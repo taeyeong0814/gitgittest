8099e9d5d9c6b7a75fa5ab220b5d5440902d0bc7
lhlhg <lhlhg@DESKTOP-2ICJNA9>
2019-08-29 20:47:31 +0900

    Revert "add print message"
    This reverts commit d2aac80b316e730385426a530c553ca562fea04d.

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -5,8 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("내가 만든 git!");
-		
-		System.out.println("아니요 재미 없어요 헿");
 	}
 
 }
