3514df8cf3944158a792f4e691abeaa9b18cd663
lhlhg <lhlhg@DESKTOP-2ICJNA9>
2019-08-29 20:51:15 +0900

    메인과 person 저장

---
+++ gittest/src/com/git/main/Main.java
@@ -0,0 +1,12 @@
+package com.git.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("내가 만든 git!");
+		
+		System.out.println("아니요 재미 없어요 헿");
+	}
+
+}
