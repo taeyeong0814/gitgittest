d2aac80b316e730385426a530c553ca562fea04d
lhlhg <lhlhg@DESKTOP-2ICJNA9>
2019-08-29 20:29:38 +0900

    add print message

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("내가 만든 git!");
+		
+		System.out.println("아니요 재미 없어요 헿");
 	}
 
 }
